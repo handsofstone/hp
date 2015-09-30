22117184a414691dd462a7c4b0f4e6a9b6d3b476
joelvoo <joel@handsofstone.ca>
2015-09-30 07:58:05 -0700

    Added Team Roster View

---
+++ HP/Controllers/TeamController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HP.Controllers
+{
+    public class TeamController : Controller
+    {
+        // GET: Team
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Roster(int id)
+        {
+            return View();
+        }
+    }
+}
