ad2c910d87ea8b3aa764c0157db4b0c851cead55
joelvoo <joel@handsofstone.ca>
2015-10-19 07:51:59 -0700

    Standings display total points

--- HP/Models/PoolViewModel.cs
+++ HP/Models/PoolViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Web;
@@ -16,14 +17,17 @@ namespace HP.Models
         public int SelectedSeasonID { get; set; }
         public IList<SelectListItem> Seasons { get; set; }
         public List<Standing> Standings { get; set; }
+        public List<StandingRow> StandingRows { get; set; }
     }
 
     public class TeamSeasonStanding
     {
+        //[ForeignKey("Team")]
         public int TeamId { get; set; }
         public int SeasonId { get; set; }
         public int Total { get; set; }
         public Team Team { get; set; }
+        public Season Season { get; set; }
     }
     public class TeamSeasonStandingMap : EntityTypeConfiguration<TeamSeasonStanding>
     {
@@ -31,7 +35,16 @@ namespace HP.Models
         {
             this.HasKey(t => new { t.TeamId, t.SeasonId });
             this.ToTable("nlpool.TeamSeasonStanding");
+            this.HasRequired(t => t.Team)
+                .WithMany(t => t.Standings)
+                .HasForeignKey(t => t.TeamId);
+            this.HasRequired(t => t.Season)
+                .WithMany()
+                .HasForeignKey(t => t.SeasonId);
         }
     }
-}
+    public class StandingRow
+    {
+
+    }
 }
