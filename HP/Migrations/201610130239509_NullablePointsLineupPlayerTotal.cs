f357b5ba1da9fc094fa94c7d9f0c3e90730105fd
Joel <joel@handsofstone.ca>
2016-10-12 22:11:56 -0700

    Standings totals fixed, but not interval gain

---
+++ HP/Migrations/201610130239509_NullablePointsLineupPlayerTotal.cs
@@ -0,0 +1,16 @@
+namespace HP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NullablePointsLineupPlayerTotal : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- HP/Scripts/standings.js
+++ HP/Scripts/standings.js
@@ -1,15 +1,39 @@
-﻿$(document).ready(function () {
+﻿String.prototype.compose = (function () {
+    var re = /\{{(.+?)\}}/g;
+    return function (o) {
+        return this.replace(re, function (_, k) {
+            return typeof o[k] != 'undefined' ? o[k] : '';
+        });
+    }
+}());
+
+function populateStandings(standings) {
+    $('#standingsTable').children('tbody').empty();
+    $.each(standings, function (i, standing) {
+        $('#standingsTable').children('tbody').append(standingsRow(standing));
+    });
+}
+
+function standingsRow(obj) {
+    var row = '<tr>' +
+    '<td>{{Rank}}</td>' +
+    '<td>{{Name}}</td>' +
+    '<td>{{Gain}}</td>' +
+    '<td>{{Total}}</td>' +
+    '</tr>';
+    return row.compose(obj);
+}
+$(document).ready(function () {
     //Dropdownlist Selectedchange event
     $("#SelectedSeasonID").change(function () {
         $.ajax({
             contentType: 'application/json, charset=utf-8',
             type: 'GET',
-            url: '/Team/StandingRows', // we are calling json method
+            url: '/Pool/StandingRows', // we are calling json method
             dataType: 'json',
             data: { poolId: $('#SelectedPoolId').val(), seasonId: $("#SelectedSeasonID").val() },
             success: function (standings) {
-                $('#lineup').empty();
-                $('#intervalStart').text(date.replace(/\"/g, ""));
+                populateStandings(standings);
             },
             error: function (ex) {
                 alert('Failed to retrieve start time.' + ex);
