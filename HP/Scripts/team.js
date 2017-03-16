44ed553ce5e8bd9f0d849ec7ecaabdec85ccde22
Joel Voo <joel@handofstone.ca>
2017-03-16 16:51:17 -0700

    updates to trade dashboard

--- HP/Scripts/team.js
+++ HP/Scripts/team.js
@@ -471,3 +471,11 @@ $(function () {
 
     $(".droppable").disableSelection();
 });
+
+$('#offersTable').on('click', '.clickable-row', function (event) {
+    if ($(this).hasClass('active')) {
+        $(this).removeClass('active');
+    } else {
+        $(this).addClass('active').siblings().removeClass('active');
+    }
+});
