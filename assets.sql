f0feb4aac54a48f71fea76d6e33182661e7f2123
Joel Voo <joel@handofstone.ca>
2017-03-17 16:58:42 -0700

    assets query

---
+++ assets.sql
@@ -0,0 +1,6 @@
+﻿select 'player' 'Type', np.ID, CONCAT (np.LAST_NAME, ', ',np.FIRST_NAME) 'Description'
+from RosterPlayer rp join NHL_PLAYER np on rp.PlayerId = np.ID
+where rp.TeamId = 2
+union 
+select 'pick' 'Type', Id, Pick 'Description' from OwnedPicks(2)
+order by Description;
